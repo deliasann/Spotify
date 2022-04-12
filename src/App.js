fb843ad66a29633cae6beeb822593a16fc2cf250
Adelia Sannomiya <adelia.sannomiya@gmail.com>
2022-04-13 00:08:59 +0700

    new file

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
+import CreatePlaylist from "./Pages/CreatePlaylist";
+import Dashboard from "./Pages/Dashboard";
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<Dashboard/>
+				</Route>
+				<Route path="/create-playlist" exact>
+					<CreatePlaylist/>
+				</Route>
+			</Switch>  
+		</Router>
+	);
+}
+
+export default App;
